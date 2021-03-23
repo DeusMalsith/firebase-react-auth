fdde45bde90f585bb2a1f88f4a589fe7b59ac5e1
DeusMalsith <justinrashallphotography@gmail.com>
2021-03-22 21:25:17 -0500

    Added sign up card

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,17 @@
+import { Container } from "react-bootstrap";
+import Signup from "./Signup";
 
 function App() {
-  return "Hello World"
+  return (
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: '400px' }}>
+        <Signup />
+      </div>
+    </Container>
+  )
 }
 
 export default App;
